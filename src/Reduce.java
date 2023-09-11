2d1c79f72f0793294ff33d17e1404de6020e5757
mottunad <nadim.mottu@mail.utoronto.ca>
2023-09-11 18:26:45 -0400

    commit!!

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 100;
+        while (n > 0) {
+            count ++;
+            n = reduce_func(n);
+        }
+        System.out.println(count);
+    }
+    public static int reduce_func(int n) {
+        if (n % 2 == 0) {
+            return (int) n / 2;
+        } else {
+            return n - 1;
+        }
+    }
 }
